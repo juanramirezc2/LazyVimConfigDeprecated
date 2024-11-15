5ad0b82d645802c8e849103f70fa11ab5ba3c9d1
Juan Ramirez <jugaramirez@utp.edu.co>
2024-11-15 11:39:32 -0500

    feat: blame current line custom config

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -262,6 +262,15 @@ return {
         topdelete = { text = "" },
         changedelete = { text = "▎" },
       },
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
       on_attach = function(buffer)
         local gs = package.loaded.gitsigns
 
